d3f547964a5fceb0b40fde505046a4357432d732
DamianoMura <mura.damiano14@gmail.com>
2025-07-01 18:54:51 +0200

    [upd] cleaned up and commented js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,29 +31,39 @@ let humanDeclared;
 let pcScore=0;
 let humanScore=0;
 let result;
-let form;
+
 
 
 
 document.getElementById("game").addEventListener("submit", function (e) {
   e.preventDefault();
+  //value coming from radio represents the number you put with your hand
   humanNumber=parseInt(document.getElementById("game").elements.namedItem("handValue").value);
+  //value coming from radio represents the prediction of the number the opponent shows with his hand
   humanDeclared=parseInt(document.getElementById("game").elements.namedItem("predicted").value);
+//adding the prediction of the opponent number with your number, represent the number you say by voice that has to be the sum by both hands
   humanDeclared+=humanNumber;
+  //random numbers for PC
   pcNumber=Math.floor(Math.random()*5)+1;
   pcDeclared=(Math.floor(Math.random()*5)+1)+pcNumber;
-  round++;//aumentiamo il numero di round e mostriamo il punteggio aggiornato
- 
+  round++;//aumentiamo il numero di round
+  //show n round number in main page
   document.getElementById('round').innerHTML = round;
+  //show round number in modal
   document.getElementById('round-v2').innerHTML =" "+round;
+
+  //modal visuals
   document.getElementById('humanDeclared').innerHTML = humanDeclared;
   document.getElementById('pcDeclared').innerHTML = pcDeclared;
+  //change the images based on the number chosen
   let humanHand = document.getElementById('humanHand');
     humanHand.src = hands[humanNumber];
   let pcHand = document.getElementById('pcHand');
     pcHand.src = hands[pcNumber];
-  
+  //calculate results
     result=pcNumber+humanNumber;
+
+    //elaborate conditions to determine who scored (if u guess the sum of the two hands you score unless you both guess it)
     if(humanDeclared != pcDeclared && humanDeclared==result){
       humanScore++;
       document.getElementById('result').innerHTML = "Hai fatto un punto"
@@ -71,7 +81,7 @@ document.getElementById("game").addEventListener("submit", function (e) {
       document.getElementById('result').innerHTML = "Nessun punto assegnato"
         
     }
-
+    //print updated score
     document.getElementById('humanScore').innerHTML = humanScore;
     document.getElementById('pcScore').innerHTML = pcScore;
 
